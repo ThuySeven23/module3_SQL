4d8f1d51c14993c64761c304e2e809226fa1fdb7
ht304 <96908813+ThuySeven23@users.noreply.github.com>
2023-05-12 14:53:13 +0700

    Update bai_tap_quan_li_sinh_vien.sql

--- 3.Thao tác CSDL/bai_tap_quan_li_sinh_vien.sql
+++ 3.Thao tác CSDL/bai_tap_quan_li_sinh_vien.sql
@@ -29,7 +29,24 @@ from class c
 where s.student_name like ""
 ;
 -- Hiển thị tất cả các thông tin môn học có credit trong khoảng từ 3-5.
-
+select sb.sub_name , sb.credit
+from `subject` sb 
+where sb.credit between 3 and 5
+;
 -- Thay đổi mã lớp(ClassID) của sinh viên có tên ‘Hung’ là 2.
+update student s
+set s.class_id = 2
+where s.student_name = "Hung"
+;
+
+select *
+from student
+; 
 -- Hiển thị các thông tin: StudentName, SubName, Mark. Dữ liệu sắp xếp theo điểm thi (mark) giảm dần. nếu trùng sắp theo tên tăng dần.
+select s.student_name , sb.sub_name , m.mark
+from mark m
+join student s on m.mark_id = s.student_id  
+join `subject` sb on m.mark_id = sb.sub_id
+order by m.mark DESC 
+;
 
